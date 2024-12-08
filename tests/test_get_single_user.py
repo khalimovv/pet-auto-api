32a6fe4cae6f5546c964dfc0cc6b14be755ce6f0
khalimovv <darkside17official@gmail.com>
2024-12-08 23:57:49 +0500

    add few tests into main file and delete .idea directory

--- tests/test_get_single_user.py
+++ tests/test_get_single_user.py
@@ -57,3 +57,64 @@ class TestCreateUser:
                                         f"Фактический статус код: {get_status_code}")
 
         assert get_json is not None, "Пустой ответ от сервера"
+
+
+class TestUpdateUser:
+    def test_update_user_info(self):
+        update_user_request = requests.put(url='https://reqres.in/api/users/2',
+                                           json={
+                                               "name": "morpheus",
+                                               "job": "zion resident"
+                                           })
+        print(f"Роут:{update_user_request.url}")
+
+        get_response = update_user_request.json()
+        print(f"Тело ответа:{get_response}")
+
+        get_status_code = update_user_request.status_code
+        print(f"Статус код от сервера: {get_status_code}")
+
+        assert get_status_code == 200, (f"Ожидаемый статус код: 200. "
+                                        f"Фактический статус код: {get_status_code}")
+
+        assert get_response is not None, "Пустой ответ от сервера"
+
+
+
+class TestDeleteUserInfo:
+    def test_delete_user_info(self):
+
+        delete_user_info_request = requests.delete(url="https://reqres.in/api/users/2")
+        print(f"Роут: {delete_user_info_request.url}")
+
+
+        get_status_code = delete_user_info_request.status_code
+        print(f"Статус код от сервера: {get_status_code}")
+
+        assert get_status_code == 204, (f"Ожидаемый статус код: 204. "
+                                        f"Фактический статус код: {get_status_code}")
+
+
+class TestRegisterUser:
+    def test_register_user(self):
+        register_user = requests.post(url="https://reqres.in/api/register", json={
+            "email": "eve.holt@reqres.in",
+            "password": "pistol"
+        })
+        print(f"Роут: {register_user.url}")
+
+
+        get_response = register_user.json()
+        print(f"Тело ответа:{get_response}")
+
+        get_status_code = register_user.status_code
+        print(f"Статус код от сервера: {get_status_code}")
+
+        assert get_status_code == 200, (f"Ожидаемый статус код: 200. "
+                                        f"Фактический статус код: {get_status_code}")
+
+        assert get_response is not None, "Пустой ответ от сервера"
+
+
+
+
