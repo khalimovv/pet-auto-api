c89c19a4e210fc423b91d68a70391aa199d0793f
khalimovv <darkside17official@gmail.com>
2025-09-18 18:40:52 +0500

    add headers in client directly

--- lib/http_methods.py
+++ lib/http_methods.py
@@ -38,33 +38,31 @@ HTTP Methods Module
 import requests
 from config.config import BASE_URL
 
+import requests
+from config.config import BASE_URL
+
 class HttpClient:
     BASE_URL = BASE_URL
+    DEFAULT_HEADERS = {"x-api-key": "reqres-free-v1"}
 
     @classmethod
-    def get(cls, path: str, **kwargs):
-        """Отправка GET запроса по указанному пути"""
+    def _request(cls, method, path, **kwargs):
         url = f"{cls.BASE_URL}{path}"
-        response = requests.get(url, **kwargs)
-        return response
+        kwargs["headers"] = {**cls.DEFAULT_HEADERS, **kwargs.get("headers", {})}
+        return requests.request(method, url, **kwargs)
 
     @classmethod
-    def post(cls, path: str, **kwargs):
-        """Отправка POST запроса по указанному пути"""
-        url = f"{cls.BASE_URL}{path}"
-        response = requests.post(url, **kwargs)
-        return response
+    def get(cls, path, **kwargs):
+        return cls._request("GET", path, **kwargs)
 
     @classmethod
-    def put(cls, path: str, **kwargs):
-        """Отправка PUT запроса по указанному пути"""
-        url = f"{cls.BASE_URL}{path}"
-        response = requests.put(url, **kwargs)
-        return response
+    def post(cls, path, **kwargs):
+        return cls._request("POST", path, **kwargs)
 
     @classmethod
-    def delete(cls, path: str, **kwargs):
-        """Отправка DELETE запроса по указанному пути"""
-        url = f"{cls.BASE_URL}{path}"
-        response = requests.delete(url, **kwargs)
-        return response
+    def put(cls, path, **kwargs):
+        return cls._request("PUT", path, **kwargs)
+
+    @classmethod
+    def delete(cls, path, **kwargs):
+        return cls._request("DELETE", path, **kwargs)
